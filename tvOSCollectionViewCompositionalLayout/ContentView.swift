65a11decf7ca92287d0f93baa28ec94890568b53
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2024-08-21 10:50:43 +1000

    Initial Commit

---
+++ tvOSCollectionViewCompositionalLayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tvOSCollectionViewCompositionalLayout
+//
+//  Created by Nicholas Clooney on 21/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tvOSCollectionViewCompositionalLayout/tvOSCollectionViewCompositionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOSCollectionViewCompositionalLayoutApp.swift
+//  tvOSCollectionViewCompositionalLayout
+//
+//  Created by Nicholas Clooney on 21/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct tvOSCollectionViewCompositionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
